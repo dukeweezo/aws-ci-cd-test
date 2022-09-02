7ede6a11d03818d23877bcee59b4480dd81bebbf
Adam Roberts <dukeweezo@gmail.com>
2022-09-02 11:30:08 -0700

    First

---
+++ bin/ci-cd.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { CiCdStack } from '../lib/ci-cd-stack';
+
+const app = new cdk.App();
+new CiCdStack(app, 'CiCdStack', {
+  env: {
+    account: '541594771816',
+    region: 'us-west-1',
+  }
+});
+
+app.synth();
